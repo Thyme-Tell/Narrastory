8ed30f8cf8d2a7ee8b2751071fe808e091b27f4b
miamiaperoff <mia@thymeandtell.com>
2025-04-19 10:08:04 -0500

    dropzone dependency fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -20,4 +19,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      external: [
+        'react-dropzone',
+        '@supabase/supabase-js'
+      ]
+    }
+  }
 }));
